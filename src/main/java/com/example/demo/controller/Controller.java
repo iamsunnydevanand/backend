b0605452d1c7eeec67acb9d2f7a6b5370a478011
iamsunnydevanand <itsmywayokok@gmail.com>
2025-03-03 12:19:26 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,73 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.intity.Intity;
+
+
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class Controller {
+
+
+
+
+
+
+
+
+    @PostMapping("/me")
+    public String login(@RequestBody Intity pro) {
+       
+       String usern;
+        usern = pro.getDev();
+        String pasm="";
+        if(usern.equals("hi")){
+
+            pasm="hi how are you sir?";
+        }
+
+        else if(usern.equals("order")){
+
+            pasm="you want to redirect to order page sir?";
+
+
+        }
+
+        else if(usern.equals("yes")){
+
+            pasm="ok i am redirecting you within 2 seconds please hold on";
+
+
+        }
+
+
+        else if(usern.equals("where is my order")){
+
+            pasm="your order will be deliver within 4 days ";
+
+
+        }
+
+        else{
+
+            pasm="i think you entered wrong input ";
+        }
+ 
+
+    
+
+        
+        return pasm;
+
+    }
+
+    
+
+
+
+}
---
+++ src/main/java/com/example/demo/intity/Intity.java
@@ -0,0 +1,20 @@
+package com.example.demo.intity;
+
+public class Intity {
+
+
+String dev;
+
+    public String getDev() {
+        return dev;
+    }
+
+    public void seDev(String dev) {
+        this.dev = dev;
+    }
+
+
+
+
+
+}
