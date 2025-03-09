190b5ee80d184a0c62735b30878229160f42e7be
Sunny Dev anand <itsmywayokok@gmail.com>
2025-03-09 19:00:28 +0530

    Update Controller.java

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -10,7 +10,7 @@ import com.example.demo.intity.Intity;
 
 
 @RestController
-@CrossOrigin(origins = "https://my-react-app-u9qm.vercel.app/agent")
+@CrossOrigin(origins = "https://my-react-app-u9qm.vercel.app")
 public class Controller {
 
 
